0a0fad3441d0d1a2ccef84b50d6264065cb93b3c
arek <bombarder1118@o2.pl>
2020-04-30 19:06:49 +0200

    add spaces

--- zadania/zad1/main.cpp
+++ zadania/zad1/main.cpp
@@ -3,6 +3,11 @@
 int main()
 {
 	char* c = NULL;
+
+
+
+
+
 	*c = 'd';
 	std::cout<<*c;
 	return 0;
--- zadania/zad2/main.cpp
+++ zadania/zad2/main.cpp
@@ -4,11 +4,9 @@ int main(){
     int i = input/2;
     int c;
     for (i>0; i--;) {
+
         c= input%i;
-        if (c==0 || i == 1)
-            std::cout << "not prime" << std::endl;
-        else
-            std::cout << "prime" << std::endl;
+
     }
     return 0;
 }

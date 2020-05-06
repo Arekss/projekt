5ad66d89e831d82e7df39f27962cc73651d27e97
arek <bombarder1118@o2.pl>
2020-05-06 20:19:58 +0200

    modifying exercise number 3 and modifying its instruction

--- zadania/zad3/main_test.cpp
+++ zadania/zad3/main_test.cpp
@@ -1,3 +1,14 @@
+/// Nalezy zmodyfikowac plik
+/// tak aby 
+/// program sie kompilowal i poprawnie wykonywal,
+/// dajac pozywtywny wynik wszystkim 3 testom.
+
+/// pomocne bedzie zajrzenie do plikow Game.cpp oraz Cell.h
+/// nie wolno modyfikowac innych plikow
+/// oraz dodawac wlasnych includow
+
+
+
 #include "gtest/gtest.h"
 #include "main.cpp"
 #include "Game.h"
@@ -5,23 +16,45 @@
 
 TEST(Test, first)
 {
-/*
+	Game game;
+	Cell currentCell;
 
- uzupelnij
+	currentCell.pos = Cell:: /*uzupelnij*/      ;    // podpowiedz: Enum
+	currentCell.alive =  /* uzupelnij */      ;      // podpowiedz: bool
+	Cell copyCell = currentCell;
 
-*/
+	game.updateCell(currentCell,copyCell,0,0);
+
+	EXPECT_EQ( /* uzupelnij */ ,copyCell.alive); // podpowiedz: bool
 }
-
-
 
 TEST(Test, second)
 {
-/*
+	Game game;
+	Cell currentCell;
 
- uzupelnij
+	currentCell.pos = Cell:: /*uzupelnij*/      ;    // podpowiedz: Enum
+	currentCell.alive =  /* uzupelnij */      ;      // podpowiedz: bool
+	Cell copyCell = currentCell;
 
-*/
-} 
+	game.updateCell(currentCell,copyCell,0,0);
+
+	EXPECT_EQ( /* uzupelnij */ ,copyCell.alive); // podpowiedz: bool
+}
+
+TEST(Test, third)
+{
+	Game game;
+	Cell currentCell;
+
+	currentCell.pos = Cell:: /*uzupelnij*/      ;    // podpowiedz: Enum
+	currentCell.alive =  /* uzupelnij */      ;      // podpowiedz: bool
+	Cell copyCell = currentCell;
+
+	game.updateCell(currentCell,copyCell,0,0);
+
+	EXPECT_EQ( /* uzupelnij */ ,copyCell.alive); // podpowiedz: bool
+}
 
 int main(int argc, char**argv)
 {
